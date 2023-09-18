1837b08ef1a85d3719734add83cc7454d6acdee7
PhamNguyenThanhTrung <trunghhf@gmail.com>
2023-09-18 21:51:48 +0700

    upload source code

---
+++ resources/views/templates/teamplate.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+
+
+</head>
+
+<body>
+
+    @yield('myhome')
+
+    <title>Resume - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    {{-- <img src="{{ asset( 'assets/img/profile.jpg') }}" alt="" class="img-1"> --}}
+    <script src="{{ asset( 'assets/js/scripts.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+</body>
+
+</html>
